4fd7b4a844b40f6d253b2a2ec378321d8214e1c3
Exequiel <exefire@gmail.com>
2015-04-03 21:51:29 -0300

    Primera Carga

---
+++ layout.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Untitled Document</title>
+<style type="text/css">
+body, html {
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+	height: 100%;
+}
+.rotar {
+	/* Safari */
+	-webkit-transform: rotate(-90deg);
+	/* Firefox */
+	-moz-transform: rotate(-90deg);
+	/* IE */
+	-ms-transform: rotate(-90deg);
+	/* Opera */
+	-o-transform: rotate(-90deg);
+	
+	/* Internet Explorer */
+	filter: progid:DXImageTransform.Microsoft.BasicImage(rotation=3);
+}
+body,td,th {
+	font-family: Helvetica, Arial, sans-serif;
+}
+h2{
+	font-weight:100;
+}
+.titulo{
+	background-color:rgba(0,0,0,0.6);
+	color:#FFF;
+	margin:3%;
+	padding:2%;
+	text-align:center;
+}
+</style>
+</head>
+
+<body>
+<div style="background:#F00; height:100%;">
+  <div style="height:5%;">Titulo</div>
+  <div style="clear:both;"></div>
+  <div style="background:#0FF; height:75%;">
+    <div style="background:#09F; width:25%; float:left; display:inline-block; height:100%;"><div class="titulo" id="titulo1">Disponible (3)</div>1</div>
+    <div style="background:#F9C; width:50%; float:left; display:inline-block; height:100%;">
+      <div style="background:#CCC; width:100%; float:left; display:inline-block; height:10%; text-align:center;">
+        <h2>CHARLIE </h2>
+      </div>
+      <div style="background:#CCC; width:10%; float:left; display:inline-block; height:80%; text-align:center;">
+        <div style="padding-top:250%;"><h2 class="rotar">BRAVO</h2></div>
+      </div>
+      <div style="background:#000; width:80%; height:80%; float:left; display:inline-block;">
+        <div style="background:#6F0; width:50%; float:left; display:inline-block; height:50%;"><div class="titulo" id="titulo2">Cuadrante</div>2</div>
+        <div style="background:#6C0; width:50%; float:left; display:inline-block; height:50%;"><div class="titulo" id="titulo3">Cuadrante</div>3</div>
+        <div style="background:#690; width:50%; float:left; display:inline-block; height:50%;"><div class="titulo" id="titulo4">Cuadrante</div>4</div>
+        <div style="background:#660; width:50%; float:left; display:inline-block; height:50%;"><div class="titulo" id="titulo5">Cuadrante</div>5</div>
+      </div>
+      <div style="background:#CCC; width:10%; float:left; display:inline-block; height:80%; text-align:center;">
+        <div style="padding-top:250%;"><h2 class="rotar">DELTA</h2></div>
+      </div>
+      <div style="background:#CCC; width:100%; float:left; display:inline-block; height:10%; text-align:center;">
+        <h2>ALPHA </h2>
+      </div>
+    </div>
+    <div style="background:#09F; width:25%; float:left; display:inline-block; height:100%;">INFO</div>
+  </div>
+  <div style="background:#0F0; height:20%;">
+    <div style="background:#00F; width:25%; float:left; display:inline-block; height:100%;"><div class="titulo" id="titulo6">Caja</div>6</div>
+    <div style="background:#00F; width:25%; float:left; display:inline-block; height:100%;"><div class="titulo" id="titulo7">Caja</div>7</div>
+    <div style="background:#00F; width:25%; float:left; display:inline-block; height:100%;"><div class="titulo" id="titulo8">Caja</div>8</div>
+    <div style="background:#00F; width:25%; float:left; display:inline-block; height:100%;"><div class="titulo" id="titulo9">Caja</div>9</div>
+  </div>
+</div>
+</body>
+</html>
